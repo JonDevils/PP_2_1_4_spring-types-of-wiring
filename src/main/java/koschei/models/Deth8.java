c75f5e3337dfe668bc865e27385482c68b7d38b3
JonDevils <jondevils@yandex.ru>
2023-01-19 20:34:44 +0300

    1

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,14 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+    public Deth8() {
+    }
+
+    @Override
+    public String toString() {
+        return "товарищ приказал долго жить";
+    }
+}
